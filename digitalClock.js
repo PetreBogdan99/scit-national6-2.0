7dfe84d120bd4e8aeae629e42a8a8bbe1ff5466c
petrebogdan99 <petre.bogdan1@yahoo.com>
2021-03-06 01:24:08 +0200

    finished homework

--- digitalClock.js
+++ digitalClock.js
@@ -1,30 +1,93 @@
-console.log("JavaScript - AJAX");
-console.log("CRUD Operation - Create");
-console.log("Create Read Update Delete  - CRUD");
+console.log("JavaScript - Digital Clock");
+// Using the new knowledge leaned in index.js file we can implement a digital clock
+// The digital clock will use existing html objects in index.html file
+// Using "setInterval" we will calculate the value for seconds, minutes and hours an update the page every second to reflect the time spend on the page
 
-document
-  .getElementById("add-article-button")
-  .addEventListener("click", function () {
-    const articleTitle = document.getElementById("article-title").value;
-    console.log("articleTitle", articleTitle);
+let seconds = 0;
+const secondsParagraphs = document.querySelectorAll(".seconds p");
 
-    const articleContent = document.getElementById("article-content").value;
-    console.log("articleContent", articleContent);
-    if (articleTitle && articleContent) {
-      const payload = {
-        title: articleTitle,
-        content: articleContent,
-      };
+let minutes = 0;
+const minutesParagraphs = document.querySelectorAll(".minutes p");
 
-      console.log("Payload:", payload);
-      console.log("Payload Text:", JSON.stringify(payload));
+let hours = 0;
+const hoursParagraphs = document.querySelectorAll(".hours p");
 
-      fetch("https://simple-json-server-scit.herokuapp.com/posts", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(payload), // body data type must match "Content-Type" header
-      }).then(getData);
-    }
-  });
+let globalTime;
+
+function stopWatch() {
+  renderDigits(seconds, secondsParagraphs);
+  renderDigits(minutes, minutesParagraphs);
+  renderDigits(hours, hoursParagraphs);
+
+  seconds++;
+
+  if (seconds === 60) {
+    seconds = 0;
+    minutes++;
+  }
+
+  if (minutes === 60) {
+    minutes = 0;
+    hours++;
+  }
+
+  if (hours === 24) {
+    hours = 0;
+  }
+}
+
+function renderDigits(nr, pList) {
+  const stringDigits = nr + "";
+  const digitList = stringDigits.split("");
+
+  if (digitList.length === 2) {
+    pList[0].innerText = digitList[0];
+    pList[1].innerText = digitList[1];
+  } else {
+    pList[0].innerText = 0;
+    pList[1].innerText = digitList[0];
+  }
+}
+
+function start() {
+  globalTime = setInterval(stopWatch, 10);
+}
+
+function reset() {
+  clearInterval(globalTime);
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+  stopWatch();
+}
+
+function save() {
+  let savedTime = document.getElementById("saved-times");
+  savedTime.innerHTML +=
+    "<p>" +
+    " " +
+    hoursParagraphs[0].innerHTML +
+    hoursParagraphs[1].innerHTML +
+    ":" +
+    minutesParagraphs[0].innerHTML +
+    minutesParagraphs[1].innerHTML +
+    ":" +
+    secondsParagraphs[0].innerHTML +
+    secondsParagraphs[1].innerHTML;
+}
+
+function stop() {
+  clearInterval(globalTime);
+}
+
+let startButton = document.getElementById("start");
+startButton.addEventListener("click", start);
+
+let stopButton = document.getElementById("stop");
+stopButton.addEventListener("click", stop);
+
+let resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", reset);
+
+let saveButton = document.getElementById("save");
+saveButton.addEventListener("click", save);
--- index.js
+++ index.js
@@ -1,48 +1,57 @@
-console.log("JavaScript - Digital Clock");
-// Using the new knowledge leaned in index.js file we can implement a digital clock
-// The digital clock will use existing html objects in index.html file
-// Using "setInterval" we will calculate the value for seconds, minutes and hours an update the page every second to reflect the time spend on the page
+console.log("JavaScript - Async Programming");
 
-let seconds = 0;
-const secondsParagraphs = document.querySelectorAll(".seconds p");
+// To identify easily part for the code will run asynchronous you will have to look to functions that ar bound by an event
+// Every function bound to and event will be stored in a que of function that will be brought back for usage when the event is triggered
+// The functions bound to events are called "callback-functions"
 
-let minutes = 0;
-const minutesParagraphs = document.querySelectorAll(".minutes p");
+document.addEventListener("click", onClick);
 
-let hours = 0;
-const hoursParagraphs = document.querySelectorAll(".hours p");
+let counter = 0;
+// "onClick" function is a callback function and as you above can see is bound to be called by "click" event
+// even thou a callback function is stored in a que to be later used, the function still has access to the global variables/constant declared
+// in this case "counter" variable will be used from global context in the function to be incremented each time the user clicks in the document
+function onClick() {
+  console.log("just a click in the app");
+  counter++;
+  console.log("counter:", counter);
+}
+console.log("counter:", counter);
 
-setInterval(function () {
-  renderDigits(seconds, secondsParagraphs);
-  renderDigits(minutes, minutesParagraphs);
-  renderDigits(hours, hoursParagraphs);
+// "setTimeout" has the functionality of calling a callback-function with a delay
+// the first argument in this case is a anonymous function or in other terms a function without a name
+setTimeout(function () {
+  console.log("1");
+}, 2000);
+console.log("2");
 
-  seconds++;
+// yu don't need to have a anonymous function as an argument it can be a classic named function
+setTimeout(setTimeoutFunction, 2000);
 
-  if (seconds === 60) {
-    seconds = 0;
-    minutes++;
-  }
+function setTimeoutFunction() {
+  console.log("3");
+}
 
-  if (minutes === 60) {
-    minutes = 0;
-    hours++;
-  }
+// the below example will show the order of how the instruction all called
+// even thou the delay for the first and second timeout are the same "Second" and "Third will be printed in this order"
+// as is stated above we have a que of callback-functions so in case we have a event that triggers callbacks at the same time, the oldest function added in que will be the first to be called back.
+console.log("First");
+setTimeout(function () {
+  console.log("Second");
+}, 0);
+setTimeout(function () {
+  console.log("Third");
+}, 0);
+console.log("Fourth");
+setTimeout(function () {
+  console.log("Fifth");
+}, 1);
 
-  if (hours === 24) {
-    hours = 0;
-  }
+// "setInterval" has the functionality of calling the callback-function at a set interval
+// for this case it will be printed to the console "Ping" every 1000 milliseconds or 1 second
+const pingReference = setInterval(function () {
+  console.log("ping");
 }, 1000);
 
-function renderDigits(nr, pList) {
-  const stringDigits = nr + "";
-  const digitList = stringDigits.split("");
-
-  if (digitList.length === 2) {
-    pList[0].innerText = digitList[0];
-    pList[1].innerText = digitList[1];
-  } else {
-    pList[0].innerText = 0;
-    pList[1].innerText = digitList[0];
-  }
-}
+document.getElementById("stop-ping").addEventListener("click", function () {
+  clearInterval(pingReference);
+});
