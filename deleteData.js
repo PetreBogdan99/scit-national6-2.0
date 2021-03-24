a12b48df9010f7597e10a1047a1d6ca8d86411ff
petrebogdan99 <petre.bogdan1@yahoo.com>
2021-03-25 01:43:16 +0200

    finished homework

---
+++ deleteData.js
@@ -0,0 +1,7 @@
+document.getElementById("deleteData").addEventListener("click", clearData);
+
+function clearData() {
+  fetch("https://simple-json-server-scit.herokuapp.com/todo/pbogdan", {
+    method: "DELETE",
+  });
+}
--- index.js
+++ index.js
@@ -1,31 +1,133 @@
 console.log("JS-To-Do-Homework");
 
-const user = "pbogdan";
+const toDoListDiv = document.querySelector(".toDoList");
 
-const todoList = document.querySelector(".todo-input");
+fetch("https://simple-json-server-scit.herokuapp.com/todo", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    id: "pbogdan",
+    todo: [],
+  }),
+}).then(getData);
 
-const firstPayload = {
-  id: `${user}`,
-  todo: [],
-};
-
-function getTodos(user) {
-  fetch(`https://simple-json-server-scit.herokuapp.com/todo/${user}`).then(
-    (response) => {
-      if (response.status == 404) {
-        console.log("NO USER FOUND! --- creating the first POST");
-        fetch("https://simple-json-server-scit.herokuapp.com/todo", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(firstPayload),
-        });
-      } else {
-        console.log("user exists");
-      }
-    }
-  );
+function handleFetchResponse(response) {
+  return response.json();
 }
 
-getTodos(user);
+function getData() {
+  fetch("https://simple-json-server-scit.herokuapp.com/todo/pbogdan")
+    .then(handleFetchResponse)
+    .then(renderList)
+    .catch(() => {});
+}
+
+document.getElementById("addButton").addEventListener("click", addNewTodo);
+
+function addNewTodo() {
+  fetch("https://simple-json-server-scit.herokuapp.com/todo/pbogdan")
+    .then(handleFetchResponse)
+    .then(updateList)
+    .then(updateServer);
+}
+
+function updateList(response) {
+  let payload = response;
+
+  const input = document.getElementById("addInput");
+  if (input.value) {
+    let newToDo = {
+      checked: false,
+      item: input.value,
+    };
+    payload.todo.push(newToDo);
+    input.value = "";
+  }
+
+  return payload;
+}
+
+function updateServer(newPayload) {
+  fetch("https://simple-json-server-scit.herokuapp.com/todo/pbogdan", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newPayload),
+  }).then(getData);
+}
+
+function renderList(response) {
+  toDoListDiv.innerHTML = "";
+  for (element of response.todo) {
+    displayTask(element, response.todo.indexOf(element));
+  }
+}
+
+function displayTask(element, index) {
+  const createDiv = document.createElement("div");
+  createDiv.classList.add("toDoDiv");
+  createDiv.setAttribute("id", index);
+  const checkBox = document.createElement("input");
+  checkBox.setAttribute("type", "checkbox");
+  checkBox.classList.add("toDoCheckbox");
+  checkBox.addEventListener("change", status);
+  const taskName = document.createElement("h3");
+  taskName.classList.add("taskName");
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("deleteButton");
+  deleteBtn.addEventListener("click", removeTask);
+  const deleteImg = document.createElement("img");
+  deleteImg.setAttribute("src", "delete.png");
+  deleteBtn.appendChild(deleteImg);
+  taskName.innerText = element.item;
+  checkBox.checked = element.checked;
+  checkBox.name = element.item;
+  createDiv.appendChild(checkBox);
+  createDiv.appendChild(taskName);
+  createDiv.appendChild(deleteBtn);
+  toDoListDiv.prepend(createDiv);
+}
+
+function status() {
+  fetch("https://simple-json-server-scit.herokuapp.com/todo/pbogdan")
+    .then(handleFetchResponse)
+    .then(updateTask);
+}
+
+function updateTask(response) {
+  let payload = response;
+
+  let box = document.querySelectorAll(":checked");
+
+  for (const element of payload.todo) {
+    element.checked = false;
+  }
+  for (const element of box) {
+    payload.todo[element.parentNode.id].checked = true;
+  }
+
+  updateServer(payload);
+}
+
+function removeTask() {
+  this.parentNode.remove();
+  fetch("https://simple-json-server-scit.herokuapp.com/todo/pbogdan")
+    .then(handleFetchResponse)
+    .then(cleanResponse);
+}
+
+function cleanResponse(response) {
+  let payload = response;
+  let newPayloadArray = [];
+
+  let remainingTasksList = document.querySelectorAll(".toDoDiv");
+
+  for (const element of remainingTasksList) {
+    newPayloadArray.push(payload.todo[element.id]);
+  }
+  payload.todo = newPayloadArray.reverse();
+  updateServer(payload);
+}
