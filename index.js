535da3d2693f7fd659c3ec05d035bc1123f76d66
petrebogdan99 <petre.bogdan1@yahoo.com>
2021-03-30 18:39:30 +0300

    need repair

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 document.getElementById("generate-spaceship").addEventListener("click", () => {
   console.log("Spaceship Succesfully Generated!");
-  const newCar = new SpaceShip();
+  const Spaceship = new SpaceShip();
 });
--- spaceShipGenerator.js
+++ spaceShipGenerator.js
@@ -11,6 +11,7 @@ class SpaceShip {
     this.runningRockets = false;
     this.position = 0;
     this.generateHtmlRef();
+    this.move(10, 10);
   }
   generateHtmlRef() {
     this.ref = document.createElement("img");
@@ -18,4 +19,74 @@ class SpaceShip {
     this.ref.classList.add("spaceship");
     document.body.appendChild(this.ref);
   }
+  move(x, y) {
+    this.x = x;
+    this.y = y;
+    this.ref.style.transform = `translate(${this.x}px, ${this.y}px)`;
+  }
+  moveUp() {
+    this.move(this.x, this.y - 25);
+  }
+
+  moveDown() {
+    this.move(this.x, this.y + 25);
+  }
+
+  moveLeft() {
+    this.move(this.x - 25, this.y);
+  }
+
+  moveRight() {
+    this.move(this.x + 25, this.y);
+  }
 }
+
+let keyUpPress = false;
+let keyDownPress = false;
+let keyLeftPress = false;
+let keyRightPress = false;
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowUp") {
+    keyUpPress = true;
+  }
+
+  if (event.key === "ArrowDown") {
+    keyDownPress = true;
+  }
+
+  if (event.key === "ArrowRight") {
+    keyRightPress = true;
+  }
+  if (event.key === "ArrowLeft") {
+    w;
+    keyLeftPress = true;
+  }
+});
+
+document.addEventListener("keyup", (event) => {
+  if (event.key === "ArrowUp") {
+    keyUpPress = false;
+  }
+
+  if (event.key === "ArrowDown") {
+    keyDownPress = false;
+  }
+  if (event.key === "ArrowRight") {
+    keyRightPress = false;
+  }
+  if (event.key === "ArrowLeft") {
+    keyLeftPress = false;
+  }
+});
+
+const player = new SpaceShip();
+
+let gameLoop = setInterval(() => {
+  console.log(keyUpPress);
+
+  if (keyUpPress) player.moveUp();
+  if (keyDownPress) player.moveDown();
+  if (keyRightPress) player.moveRight();
+  if (keyLeftPress) player.moveLeft();
+}, 50);
