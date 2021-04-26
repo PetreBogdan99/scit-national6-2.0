a0e5dd56f57ab08e81313310a82e9acb44dffa90
petrebogdan99 <petre.bogdan1@yahoo.com>
2021-04-26 12:28:18 +0300

    finished homework

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import { AppLogo } from "../AppLogo/AppLogo";
 import "./Header.css";
+import { AppMenu } from "./Menu/Menu";
 
 export function Header() {
   return (
     <div className="app-header">
+      <AppMenu />
       <AppLogo />
-      <p className="app-header__title">To Do Apppp</p>
+      <p className="app-header__title">To Do App</p>
     </div>
   );
 }
